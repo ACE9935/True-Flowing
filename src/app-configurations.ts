4e0774695bf95947aa325756ae8a4540d23ca254
Anas El mouden <anas.el-mouden@etu.u-bordeaux.fr>
2024-10-23 18:41:24 +0200

    host edit

--- src/app-configurations.ts
+++ src/app-configurations.ts
@@ -1,7 +1,7 @@
 export const configurations = {
   appName: "True Flowing",
   bluetheme: "rgb(94, 123, 238)",
-  host: "https://abt-digital.vercel.app",
+  host: "https://true-flowing.vercel.app",
   staticQrCode: "https://firebasestorage.googleapis.com/v0/b/abt-digital.appspot.com/o/40415-bdc-qr-code.webp?alt=media&token=524eddee-f350-4466-a8ad-f2759bb0c9c5",
   darkSpinner: "https://firebasestorage.googleapis.com/v0/b/abt-digital.appspot.com/o/Rolling%401.5x-1.0s-200px-200px%20(1).gif?alt=media&token=6c4c257f-c455-49fb-b9ce-adfa5335d8bd",
   lightSpinner: "https://firebasestorage.googleapis.com/v0/b/abt-digital.appspot.com/o/Rolling%401.5x-1.0s-200px-200px%20(2).gif?alt=media&token=9ec69938-06c6-457b-a2b3-38866f0a79af",

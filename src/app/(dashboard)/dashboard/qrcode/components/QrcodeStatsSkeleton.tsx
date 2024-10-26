4cac27e4f95420b854643fc47796343f1c3c9d7b
Anas El mouden <anas.el-mouden@etu.u-bordeaux.fr>
2024-10-27 01:09:52 +0200

    commit

--- src/app/(dashboard)/dashboard/qrcode/components/QrcodeStatsSkeleton.tsx
+++ src/app/(dashboard)/dashboard/qrcode/components/QrcodeStatsSkeleton.tsx
@@ -7,7 +7,7 @@ function QrcodeStatsSkeleton() {
         <Skeleton variant="rounded" height={50} width={"80%"}/>
         <Skeleton variant="rounded" height={30} width={"50%"}/>
         </div>
-    <div className="flex gap-5">
+    <div className="lg:flex-row flex-col flex gap-5">
     <Skeleton variant="rounded" height={300} width={300} />
     <div className="flex flex-col gap-5 grow">
         <Skeleton variant="rounded" height={400} />

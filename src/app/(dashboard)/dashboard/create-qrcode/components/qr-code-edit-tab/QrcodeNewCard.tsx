9ae806423d410880b191df05611f6a30d9f0ce3f
Anas El mouden <anas.el-mouden@etu.u-bordeaux.fr>
2024-10-28 22:37:40 +0100

    commit

--- src/app/(dashboard)/dashboard/create-qrcode/components/qr-code-edit-tab/QrcodeNewCard.tsx
+++ src/app/(dashboard)/dashboard/create-qrcode/components/qr-code-edit-tab/QrcodeNewCard.tsx
@@ -11,8 +11,8 @@ function QrcodeNewCard() {
             <p className="font-semibold self-start m-3 text-center">Create new QRcode</p>
             <QrCode2 sx={{fontSize:"12rem"}}/>
             <div className="flex flex-col gap-3 w-full font-bold">
-                <button onClick={()=>router.push("/dashboard/create-qrcode?type=basic")} className="rounded-full flex gap-2 hover:scale-105 hover:bg-primary-blue-dark transition-all hover:shadow-md items-center justify-center pr-8 text-white border-2 border-white p-3"><Add/>Basic</button>
-                <button onClick={()=>router.push("/dashboard/create-qrcode?type=premium")} className="rounded-full gap-2 flex hover:scale-105 transition-all border-2 border-white hover:shadow-md items-center justify-center bg-black text-white text-primary-blue p-3"><Casino/>Premium</button>
+                <button onClick={()=>router.push("/dashboard/create-qrcode?type=basic")} className="rounded-full flex gap-2 transition-all items-center bg-slate-400 hover:bg-slate-500 justify-center pr-8 text-white border-2 border-white p-3"><Add/>Basic</button>
+                <button onClick={()=>router.push("/dashboard/create-qrcode?type=premium")} className="rounded-full gap-2 flex transition-all border-2 border-white items-center justify-center bg-amber-500 hover:bg-amber-600 text-white text-primary-blue p-3"><Casino/>Premium</button>
             </div>
         </div>
      );
